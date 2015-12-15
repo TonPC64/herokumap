76e784c6e83221417b5996b6a6d3d3bda47ce77e
kingkong64 <kingkong2103@gmail.com>
2015-12-15 18:23:24 +0700

    move maker

--- public/js/index.js
+++ public/js/index.js
@@ -1,54 +1,100 @@
 angular.module('ionicApp', ['ionic', 'ngMap'])
 
-  .controller('MarkerRemoveCtrl', function ($http, $scope, $ionicLoading, $interval) {
+.controller('MarkerRemoveCtrl', function($http, $scope, $ionicLoading, $interval) {
+
     $scope.positions = []
 
-    $scope.$on('mapInitialized', function (event, map) {
-      $scope.map = map
+    $scope.$on('mapInitialized', function(event, map) {
+        $scope.map = map
     })
 
     var socket = io.connect()
-    $scope.nameuser = prompt("ระบุชื่อที่จะแสดงในแผนที่")
-    socket.emit('user',{
-      User : $scope.nameuser
+    $scope.nameuser = "Ton"
+    socket.emit('user', {
+        User: $scope.nameuser
     })
 
 
-    socket.on('push', function (data) {
-      console.log(data)
-      $scope.positions.push(data)
-      $scope.$apply()
+    socket.on('push', function(data) {
+        console.log(data)
+        $scope.positions.push(data)
+        $scope.$apply()
     })
 
-    $scope.centerOnMe = function () {
-      $scope.positions = []
+    $scope.centerOnMe = function() {
+        $scope.positions = []
 
-      $ionicLoading.show({
-        template: 'Loading...'
-      })
+        $ionicLoading.show({
+            template: 'Loading...'
+        })
 
-      navigator.geolocation.getCurrentPosition(function (position) {
-        var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
-        $scope.positions.push({lat: position.coords.latitude,lng: position.coords.longitude})
-        console.log(pos)
-        $scope.map.setCenter(pos)
-        $ionicLoading.hide()
-        socket.emit('shareLoc', {lat: position.coords.latitude,lng: position.coords.longitude,name:$scope.nameuser})
-      })
+        
+        var generateMarkers = function() {
+            $scope.positions = [];
+            var numMarkers =1; //between 4 to 8 markers
+            for (i = 0; i < numMarkers; i++) {
+              
+                $scope.positions.push({
+                    lat: $scope.loc.lat,
+                    lng: $scope.loc.lng,
+                    name: $scope.nameuser
+                });
+            }
+            console.log("vm.positions", vm.positions);
+        };
+
+        $interval(generateMarkers, 2000);
+
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+            $scope.positions.push({
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            })
+            console.log(pos)
+            $scope.map.setCenter(pos)
+            $ionicLoading.hide()
+            socket.emit('shareLoc', {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+                name: $scope.nameuser
+            })
+        })
 
     }
 
-    $interval(function () {
+    init()
+
+
+      function init(){
       if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition)
-      } else {
-        x.innerHTML = 'Geolocation is not supported by this browser.'
-      }
-    }, 1000)
+            navigator.geolocation.getCurrentPosition(initposition)
+        } else {
+            x.innerHTML = 'Geolocation is not supported by this browser.'
+        }
+    }
+    $interval(function() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition)
+        } else {
+            x.innerHTML = 'Geolocation is not supported by this browser.'
+        }
+    }, 500)
 
-    function showPosition (position) {
-      $scope.loc = {lat: position.coords.latitude,lng: position.coords.longitude}
-      $scope.$apply()
+    function showPosition(position) {
+        $scope.loc = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        }
+        $scope.$apply()
     }
 
-  })
+    function initposition(position) {
+        $scope.initloc = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        }
+        $scope.$apply()
+    }
+
+})
